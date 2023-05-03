d3b00679b55050a0b92d32dba8ca96233e20ad54
benshabbat <benshabbat27@gmail.com>
2023-05-03 14:29:27 +0300

    add next-auth

--- e-commerce-admin/pages/index.js
+++ e-commerce-admin/pages/index.js
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-  <div className="bg-blue-900 w-screen h-screen">
-
-  </div>
-  )
+    <div className="bg-blue-900 w-screen h-screen flex items-center">
+      <div className="text-center w-full">
+        <button className="bg-white p-2 px-4 rounded-md">Login with Google</button>
+      </div>
+    </div>
+  );
 }
