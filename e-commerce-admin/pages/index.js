64e76937352f455a8a93ac022d4f67a34d1faec0
benshabbat <benshabbat27@gmail.com>
2023-05-03 15:26:14 +0300

    connection with mongodb and google oauth

--- e-commerce-admin/pages/index.js
+++ e-commerce-admin/pages/index.js
@@ -1,8 +1,17 @@
+import { useSession, signIn, signOut } from "next-auth/react"
 export default function Home() {
+
+  const { data: session } = useSession()
+  if(session) {
+    return <>
+      Signed in as {session.user.email} <br/>
+      <button onClick={() => signOut()}>Sign out</button>
+    </>
+  }
   return (
     <div className="bg-blue-900 w-screen h-screen flex items-center">
       <div className="text-center w-full">
-        <button className="bg-white p-2 px-4 rounded-md">Login with Google</button>
+        <button onClick={() => signIn("google")} className="bg-white p-2 px-4 rounded-md">Login with Google</button>
       </div>
     </div>
   );
