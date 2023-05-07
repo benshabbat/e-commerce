25722e54ed783dd9135abfde78f9e95fa26f8e73
benshabbat <benshabbat27@gmail.com>
2023-05-07 12:02:47 +0300

    some css on icons

--- e-commerce-admin/components/Nav.jsx
+++ e-commerce-admin/components/Nav.jsx
@@ -5,26 +5,26 @@ const Nav = () => {
   return (
     <aside className="text-white p-4">
       <Link href={"/"} className="flex">
-        <FaStore />
+        <FaStore className="h-6 w-6 mr-2" />
         <span>
           E commerce Admin
         </span>
       </Link>
       <nav>
         <Link href={"/"} className="flex">
-          <FaHome/>
+          <FaHome className="h-6 w-6 mr-2"/>
         Dashboard
         </Link>
         <Link href={"/orders"} className="flex">
-          <FaClipboardList/>
+          <FaClipboardList className="h-6 w-6 mr-2"/>
         Orders
         </Link>
         <Link href={"/products"} className="flex">
-          <FaBoxOpen/>
+          <FaBoxOpen className="h-6 w-6 mr-2"/>
         Products
         </Link>
         <Link href={"/settings"} className="flex">
-          <FaCog/>
+          <FaCog className="h-6 w-6 mr-2"/>
         Settings
         </Link>
       </nav>
