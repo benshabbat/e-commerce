5472d4babb0f5f54ce4300f69a7da9860a5c1192
benshabbat <benshabbat27@gmail.com>
2023-05-07 13:57:35 +0300

    name and pic for hello username

--- e-commerce-admin/components/Layout.jsx
+++ e-commerce-admin/components/Layout.jsx
@@ -1,24 +1,25 @@
-import React, { Children } from 'react'
+import React, { Children } from "react";
 import Login from "@/components/Login";
 
 import Nav from "@/components/Nav";
 import { useSession } from "next-auth/react";
-const Layout = ({children}) => {
-    const { data: session } = useSession();
-    if (!session) {
-      return (
-        <div className="bg-blue-900 w-screen h-screen flex items-center">
-          <Login />
-        </div>
-      );
-    }
+const Layout = ({ children }) => {
+  const { data: session } = useSession();
+  if (!session) {
     return (
-      <div className="bg-blue-900 min-h-screen flex">
-        <Nav />
-        
-        <div className="bg-white flex-grow mt-3 mr-3 mb-3 rounded-lg p-4">  {children}</div>
+      <div className="bg-blue-900 w-screen h-screen flex items-center">
+        <Login />
       </div>
     );
-}
+  }
+  return (
+    <div className="bg-blue-900 min-h-screen flex">
+      <Nav />
+      <div className="bg-white flex-grow mt-3 mr-3 mb-3 rounded-lg p-4">     
+        {children}
+      </div>
+    </div>
+  );
+};
 
-export default Layout
+export default Layout;
--- e-commerce-admin/pages/index.js
+++ e-commerce-admin/pages/index.js
@@ -1,10 +1,22 @@
 import Layout from "@/components/Layout";
-
+import { useSession } from "next-auth/react";
 export default function Home() {
-
+  const { data: session } = useSession();
   return (
-<Layout>
-  test
-</Layout>
+    <Layout>
+      <div className="text-blue-900 flex justify-between ">
+        <h2>
+          Hello <b>{session?.user?.name}</b>
+        </h2>
+        <div className="flex gap-1">
+          <img
+            src={session?.user?.image}
+            alt="imgAccount"
+            className="w-16 h-16 rounded-md"
+          />
+          {/* {session?.user?.name} */}
+        </div>
+      </div>
+    </Layout>
   );
 }
