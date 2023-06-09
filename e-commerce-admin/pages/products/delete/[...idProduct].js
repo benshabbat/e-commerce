fedb363a0e5c093412682b1b7016d2583f5b77cd
benshabbat <benshabbat27@gmail.com>
2023-06-09 10:50:14 +0300

    ui on delete product

--- e-commerce-admin/pages/products/delete/[...idProduct].js
+++ e-commerce-admin/pages/products/delete/[...idProduct].js
@@ -1,9 +1,32 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import Layout from "@/components/Layout";
+import { useRouter } from "next/router";
+import axios from "axios";
 const DeleteProduct = () => {
-  return (
-    <div>Deleted this Product</div>
-  )
-}
+  const [formData, setFormData] = useState();
+  const router = useRouter();
+  const { idProduct } = router?.query;
 
-export default DeleteProduct
+  const goBack = () => {
+    router.push("/products");
+  };
+
+  useEffect(() => {
+    if (idProduct) {
+      const fetchData = async () => {
+        const { data } = await axios.get("/api/products?id=" + idProduct);
+        setFormData(data);
+      };
+      fetchData();
+    }
+  }, [idProduct]);
+  return (
+    <Layout>
+      <h1>Do you really want to delete "{formData?.name}"?</h1>
+      <button>Yes</button>
+      <button onClick={goBack}>No</button>
+    </Layout>
+  );
+};
+
+export default DeleteProduct;
