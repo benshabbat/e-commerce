8c52f68a44645942cbc14be7369b0759a6104511
benshabbat <benshabbat27@gmail.com>
2023-06-26 21:59:11 +0300

    start with multiparty

--- e-commerce-admin/pages/api/upload.js
+++ e-commerce-admin/pages/api/upload.js
@@ -1,6 +1,11 @@
 import multiparty from "multiparty";
 
-export default async function handle(req, res) {}
+export default async function handle(req, res) {
+  const form = multiparty.Form()
+  form.parse(req,async (req, res) => {
+    
+  })
+}
 
 export const config = {
   api: { bodyParser: false },
