d4c8214d9861dfa3f61f494338bb9bc841248d7b
benshabbat <benshabbat27@gmail.com>
2023-08-28 22:34:12 +0300

    install mime types and create send file to bucket

--- e-commerce-admin/pages/api/upload.js
+++ e-commerce-admin/pages/api/upload.js
@@ -1,10 +1,38 @@
 import multiparty from "multiparty";
-import { S3Client } from "@aws-sdk/client-s3";
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import fs from "fs";
+import mime from "mime-types";
+const bucketName = "benshabbat-next-ecommerce";
+
 export default async function handle(req, res) {
   const form = new multiparty.Form();
   form.parse(req, async (err, fields, files) => {
     console.log(files.file.length);
-    res.json("ok");
+    const client = new S3Client({
+      region: "eu-north-1",
+      credentials: {
+        accessKeyId: process.env.S3_ACCESS_KEY,
+        secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+      },
+    });
+    const links = [];
+    for (const file of files.file) {
+      const ext = file.originalFilename.split(".").pop();
+      const newFileName = Date.now() + "." + ext;
+      console.log({ ext, file });
+      await client.send(
+        new PutObjectCommand({
+          Bucket: bucketName,
+          Key: newFileName,
+          fs: fs.readFileSync(file.path),
+          ACL: "public-read",
+          ContentType: mime.lookup(file.path),
+        })
+      );
+      const link = `https://${bucketName}.s3.amazonaws.com/${newFileName}`;
+      links.push(link);
+    }
+    res.json({links});
   });
 }
 
